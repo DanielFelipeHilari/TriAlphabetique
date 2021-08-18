7dfe20651600bb72b4f1354c8ed449050225a744
DanielFelipeHilari <danielhilari261@gmail.com>
2021-08-18 10:55:25 +0200

    Creation de la liste

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(int argc , char *argv[])
 {
 	printf("Bienvenue au programme de tri alphabetique \n");
+	char chaine[20];
+	FILE* fichier = NULL; 
+	fichier = fopen("liste.txt","r");
+	if(fichier == NULL)
+	{
+		fprintf(stderr, "Ne peut pas ouvrir le fichier \n");
+		exit(EXIT_FAILURE);
+	}
+	fgets(chaine,20,fichier);
+	printf("%s",chaine);
+	fclose(fichier);
+	
 	return 0;
 }
