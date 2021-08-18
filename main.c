62a1390ad29ba6edd2e4955aa9ee3d563e8c6d5e
DanielFelipeHilari <danielhilari261@gmail.com>
2021-08-18 13:04:07 +0200

    Creation du tableau a partir du fichier liste.txt

--- main.c
+++ main.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char** CreationTab(FILE* fichier, int nbrprenom)
+{
+	char** chaine = malloc(nbrprenom*sizeof(char*));
+	for(int i = 0; i<nbrprenom; i++)
+	{
+		chaine[i] = malloc(20*sizeof(char));
+	}
+	int i = 0;
+	int j = 0;
+	chaine[i][j] = fgetc(fichier);
+	printf("%c",chaine[i][j]);
+	while(chaine[i][j] != EOF)
+	{
+		if(chaine[i][j] == ' ')
+		{
+			chaine[i][j] = '\0';
+			i++;
+			j = 0;
+		}else
+		{
+			j++;
+			chaine[i][j] = fgetc(fichier);
+		}
+	}
+	return chaine;
+}
+
+int NombrePrenom(FILE* fichier)
+{
+	int nbm = 0;
+	char a;
+	a = fgetc(fichier);
+	while(a != EOF)
+	{
+		if( a == ' ')
+			nbm++;
+		a = fgetc(fichier);	
+	}
+	nbm++;
+	return nbm;
+}
 
 int main(int argc , char *argv[])
 {
 	printf("Bienvenue au programme de tri alphabetique \n");
-	char chaine[20];
+	//char chaine[20];
 	FILE* fichier = NULL; 
 	fichier = fopen("liste.txt","r");
 	if(fichier == NULL)
@@ -13,8 +54,21 @@ int main(int argc , char *argv[])
 		fprintf(stderr, "Ne peut pas ouvrir le fichier \n");
 		exit(EXIT_FAILURE);
 	}
-	fgets(chaine,20,fichier);
-	printf("%s",chaine);
+	
+	/*fgets(chaine,20,fichier);
+	printf("%s \n",chaine);*/
+	int n = NombrePrenom(fichier);
+	printf("%d\n",n);
+	rewind(fichier);
+	char** tab = CreationTab(fichier,n);
+	for(int i = 0;i<n;i++)
+	{
+		for(int j =0;j<20;j++)
+		{
+			printf("%c",tab[i][j]);
+		}
+		printf("\n");
+	}
 	fclose(fichier);
 	
 	return 0;
